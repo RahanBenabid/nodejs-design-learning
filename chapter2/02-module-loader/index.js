961195690ad68736d82a99733208930b6c411b8b
RahanBenabid <rahannadime@gmail.com>
2024-12-14 12:46:52 +0100

    Some more progress

--- chapter2/02-module-loader/index.js
+++ chapter2/02-module-loader/index.js
@@ -1,3 +1,8 @@
+// this will try to emulate how CommonJS works in node, 
+// this will create a function that mimics a subset if the functionality of the `require` function in node
+
+// this function loads the content of a module into a private scope, then evaluates it
+
 
 function loadModule (filename, module, require) {
 	const wrappedSrc = 
@@ -5,4 +10,34 @@ function loadModule (filename, module, require) {
 		${fs.readFileSync(filename, 'utf-8')}
 	})(module, module.exports, require)`
 	eval(wrappedSrc)
+}
+
+function require (moduleName) {
+	console.log(`Require involked for module${moduleName}`);
+	const id = require.resolve(moduleName);
+	
+	if (require.cache[id]) {
+		return require.cache[id].exports
+	}
+	
+	// module metadata
+	const module = {
+		exports: {},
+		id
+	}
+	
+	// update the cache
+	require.cache[id] = module;
+	
+	// load the module
+	loadModule(id, module, require);
+	
+	// return exported variables
+	return module.exports;
+}
+
+require.cache = {}
+
+require.resolve = (moduleName) => {
+	// resolve a full module
 }
